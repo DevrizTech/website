06404239547f225c2e3dd7f4a428fe91a725d9f5
Prakhar Shukla <imprakharshukla@gmail.com>
2021-08-15 21:28:40 +0530

    replaced the team image links

--- components/meet.vue
+++ components/meet.vue
@@ -14,9 +14,9 @@
           <div class="flex-col">
             <div class="w-16 h-16 mb-6 rounded-full bg-blue-400">
               <img
-                class="object-cover"
-                src="https://andronix.app/wp-content/uploads/2019/11/avataaars.png"
-                alt=""
+                  class="object-cover"
+                  src="https://i.ibb.co/hVVMj3F/prakhar-avataar.png"
+                  alt=""
               />
             </div>
 
@@ -29,17 +29,17 @@
           </div>
           <div class="my-3 inline-flex gap-x-5">
             <a href="https://github.com/imprakharshukla">
-            <svg 
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="
                 feather feather-github
                 text-gray-700
                 stroke-current
@@ -50,24 +50,24 @@
                 duration-200
                 cursor-pointer
               "
-            >
-              <path
-                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-              />
-            </svg>
+              >
+                <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                />
+              </svg>
             </a>
             <a href="mailto:prakhar@devriz.com">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="
                 feather feather-at-sign
                 text-gray-700
                 stroke-current
@@ -78,10 +78,10 @@
                 duration-200
                 cursor-pointer
               "
-            >
-              <circle cx="12" cy="12" r="4" />
-              <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
-            </svg>
+              >
+                <circle cx="12" cy="12" r="4"/>
+                <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"/>
+              </svg>
             </a>
           </div>
         </div>
@@ -89,30 +89,30 @@
           <div>
             <div class="w-16 h-16 mb-6 rounded-full bg-blue-400">
               <img
-                class="object-cover"
-                src="https://andronix.app/wp-content/uploads/2019/11/avataaars-1.png"
-                alt=""
+                  class="object-cover"
+                  src="https://i.ibb.co/BCTHfFq/anu-avatar.png"
+                  alt=""
               />
             </div>
             <p class="font-bold text-2xl my-2 text-gray-700 mt-2">Anurag Verma</p>
-               <p class="text-blue-500 font-bold text-sm mt-1">Co-Founder</p>
+            <p class="text-blue-500 font-bold text-sm mt-1">Co-Founder</p>
             <p class="my-4 text-gray-600 md:max-w-sm">
-            Python, Linux, Bash and Dev Ops Expert. I sometimes enjoy JS.
+              Python, Linux, Bash and Dev Ops Expert. I sometimes enjoy JS.
             </p>
           </div>
           <div class="my-3 inline-flex gap-x-5">
-              <a href="https://github.com/ultrahacx">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="
+            <a href="https://github.com/ultrahacx">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="
                 feather feather-github
                 text-gray-700
                 stroke-current
@@ -123,24 +123,24 @@
                 duration-200
                 cursor-pointer
               "
-            >
-              <path
-                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-              />
-            </svg>
-              </a>
-                <a href="mailto:anurag@devriz.com">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="
+              >
+                <path
+                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                />
+              </svg>
+            </a>
+            <a href="mailto:anurag@devriz.com">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="
                 feather feather-at-sign
                 text-gray-700
                 stroke-current
@@ -151,11 +151,11 @@
                 duration-200
                 cursor-pointer
               "
-            >
-              <circle cx="12" cy="12" r="4" />
-              <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
-            </svg>
-                </a>
+              >
+                <circle cx="12" cy="12" r="4"/>
+                <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"/>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
@@ -163,11 +163,11 @@
   </div>
 </template>
 
- <script>
+<script>
 export default {
   name: "meet",
 };
 </script>
- 
- <style>
+
+<style>
 </style>
